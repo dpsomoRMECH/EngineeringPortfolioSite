12abbf7edcfb7106c2124c7505f635bde1c4000c
dpsomoRMECH <dpsomo@outlook.com>
2026-08-10 16:40:46 +1000

    Updating homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // IMPORTANT: replace this with your actual domain once you've connected it
 // (used for generating correct absolute URLs, e.g. in sitemaps/meta tags)
 export default defineConfig({
-  site: 'https://your-domain.com',
+  site: 'https://portfolio.deanpsomo.dev',
 });
